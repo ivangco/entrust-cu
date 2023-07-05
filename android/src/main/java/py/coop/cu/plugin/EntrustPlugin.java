72f3626e3eb08396e9594ed93d5b7bf8f342ca27
Dario Mora <d_mora@cooperativauniversitaria.coop>
2023-07-05 11:21:12 -0400

    agregar sdk de entrust

--- android/src/main/java/py/coop/cu/plugin/EntrustPlugin.java
+++ android/src/main/java/py/coop/cu/plugin/EntrustPlugin.java
@@ -1,5 +1,6 @@
 package py.coop.cu.plugin;
 
+import com.entrust.identityGuard.mobile.sdk.Identity;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
@@ -9,6 +10,8 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 @CapacitorPlugin(name = "Entrust")
 public class EntrustPlugin extends Plugin {
 
+    Identity identity = new Identity();
+
     private Entrust implementation = new Entrust();
 
     @PluginMethod
@@ -16,10 +19,11 @@ public class EntrustPlugin extends Plugin {
         String value = call.getString("value");
 
         System.out.println("valor que se le pasa al plugin -> " + value);
+        System.out.println("device id -> " + identity.getDeviceId());
 
         JSObject ret = new JSObject();
-        // ret.put("value", implementation.echo(value));
-        ret.put("value", "valor devuelto desde el plugin android");
+//         ret.put("value", implementation.echo(value));
+        ret.put("value", identity.getDeviceId());
         call.resolve(ret);
     }
 }
