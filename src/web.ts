3433d3f02122cdfcde950b4b34111fe7f99bdcc5
Dario Mora <dfma010@gmail.com>
2023-07-17 11:51:48 -0400

    implementacion de plugin para la web

--- src/web.ts
+++ src/web.ts
@@ -1,10 +1,18 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { EntrustPlugin } from './definitions';
+import type { EntrustPlugin, PluginParams } from './definitions';
 
 export class EntrustWeb extends WebPlugin implements EntrustPlugin {
-  async echo(options: { serialNumber: string, activationCode: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return { value: "echo" };
+  
+  getTokenOTP(): Promise<{ otp: string; }> {
+    throw new Error('Method not implemented.');
   }
+  activateTokenQr({ uri }: { uri: string; }): Promise<{ value: string; }> {
+    throw new Error('Method not implemented.' + uri);
+  }
+  async activateTokenQuick(options: PluginParams): Promise<{ data: string }> {
+    console.log('ECHO', options);
+    return { data: "" };
+  }
+
 }
