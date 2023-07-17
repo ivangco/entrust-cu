e046c6d3dbb9d7d56af03221a31bca986d46d423
Dario Mora <dfma010@gmail.com>
2023-07-17 11:50:05 -0400

    agregar metodos a implementar

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,9 @@
+export interface PluginParams {
+  serialNumber: string; regAddress: string; regPassword: string
+}
+
 export interface EntrustPlugin {
-  echo(options: { serialNumber: string, activationCode:string }): Promise<{ value: string }>;
+  activateTokenQuick(options: PluginParams): Promise<{ data: string }>;
+  getTokenOTP(options: { jsonIdentity: string }): Promise<{ otp: string }>
+  activateTokenQr({ uri }: { uri: string }): Promise<{ value: string }>
 }
