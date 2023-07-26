8d20f54cd6b3098e7b2a9de35137c310e421c6a8
Dario Mora <dfma010@gmail.com>
2023-07-26 16:02:09 -0400

    corregir merge

--- android/src/main/java/py/coop/cu/plugin/EntrustPlugin.java
+++ android/src/main/java/py/coop/cu/plugin/EntrustPlugin.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package py.coop.cu.plugin;
 
 import android.content.Intent;
@@ -60,34 +59,3 @@ public class EntrustPlugin extends Plugin {
 
     }
 }
-=======
-package py.coop.cu.plugin;
-
-import com.entrust.identityGuard.mobile.sdk.Identity;
-import com.getcapacitor.JSObject;
-import com.getcapacitor.Plugin;
-import com.getcapacitor.PluginCall;
-import com.getcapacitor.PluginMethod;
-import com.getcapacitor.annotation.CapacitorPlugin;
-
-@CapacitorPlugin(name = "Entrust")
-public class EntrustPlugin extends Plugin {
-
-    Identity identity = new Identity();
-
-    private Entrust implementation = new Entrust();
-
-    @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
-
-        System.out.println("valor que se le pasa al plugin -> " + value);
-        System.out.println("device id -> " + identity.getDeviceId());
-
-        JSObject ret = new JSObject();
-//         ret.put("value", implementation.echo(value));
-        ret.put("value", identity.getDeviceId());
-        call.resolve(ret);
-    }
-}
->>>>>>> cambios-ios
