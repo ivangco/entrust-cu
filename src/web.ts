1c5b3443fd310d6766d625199ffa4630eb41bc36
Dario Mora <dfma010@gmail.com>
2023-07-28 17:03:58 -0400

    implementar metodos para la web

--- src/web.ts
+++ src/web.ts
@@ -1,19 +1,27 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { EntrustPlugin, PluginParams } from './definitions';
+import type { EntrustPlugin, ActivationParams } from './definitions';
 
 export class EntrustWeb extends WebPlugin implements EntrustPlugin {
-  initializeSDK(): Promise<{response:Boolean}> {
+  completeChallenge(): Promise<{ response: boolean; }> {
     throw new Error('Method not implemented.');
   }
-  
+  listTransactions(options: { jsonIdentity: string; }): Promise<{ response: string; }> {
+    throw new Error('Method not implemented.' + options.jsonIdentity);
+  }
+  initializeSDK(): Promise<{ response: boolean }> {
+    throw new Error('Method not implemented.');
+  }
+
   getTokenOTP(): Promise<{ otp: string; }> {
     throw new Error('Method not implemented.');
   }
+
   activateTokenQr({ uri }: { uri: string; }): Promise<{ value: string; }> {
     throw new Error('Method not implemented.' + uri);
   }
-  async activateTokenQuick(options: PluginParams): Promise<{ data: string }> {
+
+  async activateTokenQuick(options: ActivationParams): Promise<{ data: string }> {
     console.log('ECHO', options);
     return { data: "" };
   }
